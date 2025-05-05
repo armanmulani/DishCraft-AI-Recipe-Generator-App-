133f6ee8888f32bb0307bb492aa40f0a55be2661
armanmulani <armanmulani2212@gmail.com>
2025-05-05 11:48:59 +0530

    main component updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,36 @@
+import React from "react"
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientList = ingredients.map(ingredient => {
+        return <li key={ingredient}>{ingredient}</li>
+    })
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+         const newIngredient= formData.get("ingredient")
+        setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+    }
     return (
         <>
-            <form className="ingredients-form">
+            <form onSubmit={handleSubmit} className="ingredients-form">
                 <input
                 type="text"
                 placeholder="e.g. Oregano"
                 aria-label="Add Ingredients"
+                name="ingredient"
                 />
-                <button>Add Ingredients</button>
+                <button >Add Ingredients</button>
             </form>
+
+            <ul>
+                {ingredientList}
+            </ul>
         </>
+
+
     )
 }
